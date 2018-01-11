5610d0e5d5f6d4a49f6825b37d48f0aa0351bafc
Daniel Dimitrov <bgdppeu@gmail.com>
2018-01-11 05:20:40 +0200

    Remove mysql

--- helpers/config.py
+++ helpers/config.py
@@ -1,13 +1,6 @@
 import json
 
 
-def mysql():
-    with open("./mysql.json", "r") as f:
-        mysql = json.load(f)
-
-    return mysql
-
-
 def flask():
     with open("./flask.json", "r") as f:
         flask = json.load(f)
