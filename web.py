2076865ccfb54c988a7e9d668882230f653e906b
Daniel Dimitrov <bgdppeu@gmail.com>
2018-01-11 05:58:26 +0200

    Add wrapper for valid token instead checking everytime...

--- web.py
+++ web.py
@@ -1,21 +1,29 @@
 from flask import Flask, jsonify, request
 from helpers import config, privileges, mysql
+from functools import wraps
 
 app = Flask(__name__)
 
 
-def valid_token(token):
-    connection, cursor = mysql.connect()
-    get_tokens = mysql.execute(connection, cursor, "SELECT token FROM access_keys WHERE token = %s", [token]).fetchone()
+def valid_token(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        connection, cursor = mysql.connect()
 
-    if get_tokens:
-        return True
+        token = request.args.get('token')
 
-    return False
+        get_tokens = mysql.execute(connection, cursor, "SELECT token FROM access_keys WHERE token = %s",
+                                   [token]).fetchone()
+
+        if get_tokens:
+            return function(*args, **kwargs)
+
+        return 'Invalid token!'
+
+    return wrapper
 
 
 def group(p):
-
     if privileges.hasuser(p):
         group = "User"
 
@@ -42,100 +50,123 @@ def not_found(error):
 
 
 @app.route('/api/full', methods=['GET'])
+@valid_token
 def api_list():
-    token = request.args.get('token')
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
 
-    if valid_token(token):
-        connection, cursor = mysql.connect()
-        user_id = request.args.get('user_id')
+    if user_id:
 
-        if user_id:
+        full = {}
 
-            full = {}
+        find_user = mysql.execute(connection, cursor,
+                                  "SELECT user_id, username, twitch_username FROM users WHERE user_id = %s",
+                                  [user_id]).fetchone()
 
-            find_user = mysql.execute(connection, cursor,
-                                      "SELECT user_id, username, twitch_username FROM users WHERE user_id = %s",
+        find_settings = mysql.execute(connection, cursor,
+                                      "SELECT * FROM settings WHERE user_id = %s",
                                       [user_id]).fetchone()
 
-            find_settings = mysql.execute(connection, cursor,
-                                          "SELECT * FROM settings WHERE user_id = %s",
-                                          [user_id]).fetchone()
+        find_tracking = mysql.execute(connection, cursor,
+                                      "SELECT * FROM tracking WHERE user_id = %s",
+                                      [user_id]).fetchone()
 
-            find_tracking = mysql.execute(connection, cursor,
-                                          "SELECT * FROM tracking WHERE user_id = %s",
-                                          [user_id]).fetchone()
+        full.update(find_user)
+        full.update(find_settings)
+        full.update(find_tracking)
+        full.update(group(find_settings["privileges"]))
 
-            full.update(find_user)
-            full.update(find_settings)
-            full.update(find_tracking)
-            full.update(group(find_settings["privileges"]))
-
-            return jsonify(full)
-        else:
-            return 'User not found!'
-
-    return 'Invalid token!'
+        return jsonify(full)
+    else:
+        return 'User not found!'
 
 
 @app.route('/api/get_user', methods=['GET'])
+@valid_token
 def api_get_user():
-    token = request.args.get('token')
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
 
-    if valid_token(token):
-        connection, cursor = mysql.connect()
-        user_id = request.args.get('user_id')
+    if user_id:
 
-        if user_id:
+        find_user = mysql.execute(connection, cursor,
+                                  "SELECT user_id, username, twitch_username FROM users WHERE user_id = %s",
+                                  [user_id]).fetchone()
 
-            find_user = mysql.execute(connection, cursor,
-                                      "SELECT user_id, username, twitch_username FROM users WHERE user_id = %s",
-                                      [user_id]).fetchone()
-
-            return jsonify(find_user)
-        else:
-            return 'User not found!'
-
-    return 'Invalid token!'
+        return jsonify(find_user)
+    else:
+        return 'User not found!'
 
 
 @app.route('/api/get_settings', methods=['GET'])
+@valid_token
 def api_get_settings():
-    token = request.args.get('token')
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
 
-    if valid_token(token):
-        connection, cursor = mysql.connect()
-        user_id = request.args.get('user_id')
+    if user_id:
 
-        if user_id:
+        find_user = mysql.execute(connection, cursor, "SELECT * FROM settings WHERE user_id = %s",
+                                  [user_id]).fetchone()
 
-            find_user = mysql.execute(connection, cursor, "SELECT * FROM settings WHERE user_id = %s",
-                                      [user_id]).fetchone()
-
-            return jsonify(find_user)
-        else:
-            return 'User not found!'
-
-    return 'Invalid token!'
+        return jsonify(find_user)
+    else:
+        return 'User not found!'
 
 
 @app.route('/api/get_tracking', methods=['GET'])
+@valid_token
 def api_get_tracking():
-    token = request.args.get('token')
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
 
-    if valid_token(token):
-        connection, cursor = mysql.connect()
-        user_id = request.args.get('user_id')
+    if user_id:
 
-        if user_id:
+        find_user = mysql.execute(connection, cursor, "SELECT * FROM tracking WHERE user_id = %s",
+                                  [user_id]).fetchone()
 
-            find_user = mysql.execute(connection, cursor, "SELECT * FROM tracking WHERE user_id = %s",
-                                      [user_id]).fetchone()
+        return jsonify(find_user)
+    else:
+        return 'User not found!'
 
-            return jsonify(find_user)
-        else:
-            return 'User not found!'
 
-    return 'Invalid token!'
+@app.route('/api/update/user')
+@valid_token
+def api_update_user():
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
+
+    if user_id:
+
+        return ''
+    else:
+        return 'User not found!'
+
+
+@app.route('/api/update/settings')
+@valid_token
+def api_update_settings():
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
+
+    if user_id:
+
+        return ''
+    else:
+        return 'User not found!'
+
+
+@app.route('/api/update/tracking')
+@valid_token
+def api_update_tracking():
+    connection, cursor = mysql.connect()
+    user_id = request.args.get('user_id')
+
+    if user_id:
+
+        return ''
+    else:
+        return 'User not found!'
 
 
 if __name__ == "__main__":
