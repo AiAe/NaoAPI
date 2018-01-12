f3b7245bdb85e59393fba9316e12f44b073c6867
AiAe <bgdppeu@gmail.com>
2018-01-12 23:56:24 +0200

    Working update requests (will get rewriten)

--- routes/api_update_user.py
+++ routes/api_update_user.py
@@ -1,15 +1,33 @@
 from flask import request, jsonify
-from helpers import mysql, user_exist
+from helpers import mysql, user_exist, ripple
 
 table = ["user_id", "username", "twitch_username", "code"]
+
 
 def api():
     user_id = request.args.get('user_id')
     update = request.args.get('update')
+    value = request.args.get('value')
 
     if user_id and user_exist.user() and update in table:
         connection, cursor = mysql.connect()
 
-        return jsonify({"code": "1", "message": "User updated!"})
+        user = ripple.get_user(user_id)
+
+        if not user["code"] == 0:
+
+            if update == "username":
+                mysql.execute(connection, cursor, "UPDATE users SET username = %s WHERE user_id = %s",
+                              [user["username"], user_id])
+            elif update == "twitch_username" and value:
+                mysql.execute(connection, cursor, "UPDATE users SET twitch_username = %s WHERE user_id = %s",
+                              [value, user_id])
+            elif update == "code" and value:
+                mysql.execute(connection, cursor, "UPDATE users SET code = %s WHERE user_id = %s",
+                              [value, user_id])
+            else:
+                return jsonify({"code": "0", "message": "Something went wrong!"})
+
+            return jsonify({"code": "1", "message": "User updated!"})
     else:
-        return jsonify({"code": "0", "message": "Bad POST request"})
+        return jsonify({"code": "0", "message": "Bad POST request!"})
