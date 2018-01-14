a23d8c2c054c4efd4fe32ca39a23d624a7cacf46
AiAe <bgdppeu@gmail.com>
2018-01-15 00:36:19 +0200

    Return everything instead

--- routes/api_get_user.py
+++ routes/api_get_user.py
@@ -9,7 +9,7 @@ def api():
     if user_id:
 
         find_user = mysql.execute(connection, cursor,
-                                  "SELECT user_id, username, twitch_username FROM users WHERE user_id = %s",
+                                  "SELECT * FROM users WHERE user_id = %s",
                                   [user_id]).fetchone()
         if find_user:
             find_user.update({"code": "1"})
