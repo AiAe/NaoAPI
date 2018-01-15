ad4eb2673522f54c90d21fa15e8d5be00fa49c4b
AiAe <bgdppeu@gmail.com>
2018-01-15 18:40:08 +0200

    Token checks

--- routes/api_twitch.py
+++ routes/api_twitch.py
@@ -6,24 +6,33 @@ import requests
 def api():
     code = request.args.get('state')
 
-    twitch_data = {
-        'client_id': config.twitch()["twitch_client"],
-        'client_secret': config.twitch()["twitch_secret"],
-        'grant_type': 'authorization_code',
-        'redirect_uri': config.twitch()["twitch_redirect"],
-        'code': request.args['code']
-    }
+    if code:
+        connection, cursor = mysql.connect()
 
-    twitch_api = requests.post("https://api.twitch.tv/kraken/oauth2/token", data=twitch_data).json()
-    headers = {
-        'Authorization': 'OAuth ' + twitch_api['access_token']
-    }
+        find_user = mysql.execute("SELECT user_id FROM users WHERE token = %s", [code]).fetchone()
 
-    user = requests.get('https://api.twitch.tv/kraken/', headers=headers).json()
+        if not find_user:
+            return jsonify({"code": "0", "message": "User not found!"})
 
-    connection, cursor = mysql.connect()
-    mysql.execute(connection, cursor,
-                  "UPDATE users SET twitch_username = %s WHERE token = %s", [user["token"]["user_name"],
-                                                                             code])
+        twitch_data = {
+            'client_id': config.twitch()["twitch_client"],
+            'client_secret': config.twitch()["twitch_secret"],
+            'grant_type': 'authorization_code',
+            'redirect_uri': config.twitch()["twitch_redirect"],
+            'code': request.args['code']
+        }
 
-    return jsonify({"code": "1", "message": "User connected to Twitch."})
+        twitch_api = requests.post("https://api.twitch.tv/kraken/oauth2/token", data=twitch_data).json()
+        headers = {
+            'Authorization': 'OAuth ' + twitch_api['access_token']
+        }
+
+        user = requests.get('https://api.twitch.tv/kraken/', headers=headers).json()
+
+        mysql.execute(connection, cursor,
+                      "UPDATE users SET twitch_username = %s WHERE token = %s", [user["token"]["user_name"],
+                                                                                 code])
+
+        return jsonify({"code": "1", "message": "User connected to Twitch."})
+
+    return jsonify({"code": "0", "message": "Something went wrong!"})
