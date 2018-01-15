0a07a804ccd5b5ca90db9c7b9bbb3446e5d5c0c4
AiAe <bgdppeu@gmail.com>
2018-01-15 18:44:14 +0200

    More messages

--- routes/api_twitch.py
+++ routes/api_twitch.py
@@ -9,7 +9,7 @@ def api():
     if code:
         connection, cursor = mysql.connect()
 
-        find_user = mysql.execute(connection, cursor, "SELECT user_id FROM users WHERE token = %s", [code]).fetchone()
+        find_user = mysql.execute(connection, cursor, "SELECT user_id, twitch_username FROM users WHERE token = %s", [code]).fetchone()
 
         if not find_user:
             return jsonify({"code": "0", "message": "User not found!"})
@@ -32,7 +32,8 @@ def api():
         mysql.execute(connection, cursor,
                       "UPDATE users SET twitch_username = %s WHERE token = %s", [user["token"]["user_name"],
                                                                                  code])
-
+        if find_user["twitch_username"]:
+            return jsonify({"code": "1", "message": "Username is updated!"})
         return jsonify({"code": "1", "message": "User connected to Twitch."})
 
     return jsonify({"code": "0", "message": "Something went wrong!"})
