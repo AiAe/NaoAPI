84de51ef4cbe65b4c3c365b6f6b10bf99d984c7f
AiAe <bgdppeu@gmail.com>
2018-01-17 00:02:50 +0200

    Add searach user by twitch username

--- routes/api_get_tracking.py
+++ routes/api_get_tracking.py
@@ -5,12 +5,18 @@ from helpers import mysql
 def api():
     connection, cursor = mysql.connect()
     user_id = request.args.get('user_id')
+    twitch = request.args.get('twitch')
+
+    if twitch:
+        find_channel = mysql.execute(connection, cursor, "SELECT user_id FROM users WHERE twitch_username = %s",
+                                     [twitch]).fetchone()
+
+        user_id = find_channel["user_id"]
 
     if user_id:
-
         find_user = mysql.execute(connection, cursor, "SELECT * FROM tracking WHERE user_id = %s",
                                   [user_id]).fetchone()
+        if find_user:
+            return jsonify(find_user)
 
-        return jsonify(find_user)
-    else:
-        return 'User not found!'
+    return 'User not found!'
