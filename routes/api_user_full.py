366f04f919257e6d5a7412735e9448d4dca16309
AiAe <bgdppeu@gmail.com>
2018-01-16 17:29:27 +0200

    Use 1 execute instead 3

--- routes/api_user_full.py
+++ routes/api_user_full.py
@@ -10,22 +10,21 @@ def api():
 
         full = {}
 
-        find_user = mysql.execute(connection, cursor,
-                                  "SELECT user_id, username, twitch_username FROM users WHERE user_id = %s",
-                                  [user_id]).fetchone()
+        find = mysql.execute(connection, cursor,
+                             '''
+                             SELECT `users`.`user_id`, `users`.`username`, `users`.`twitch_username`, `settings`.`bot`, 
+                             `settings`.`requests`, `settings`.`privileges`, `settings`.`std_pp_limit`, 
+                             `settings`.`taiko_pp_limit`, `settings`.`ctb_pp_limit`, `settings`.`mania_pp_limit`, 
+                             `settings`.`format_score_osu`, `settings`.`format_score_twitch`, 
+                             `settings`.`format_request_osu`, `settings`.`format_request_twitch`, `tracking`.`std_pp`, 
+                             `tracking`.`std_rank`, `tracking`.`taiko_pp`, `tracking`.`taiko_rank`, `tracking`.`ctb_pp`, 
+                             `tracking`.`ctb_rank`, `tracking`.`mania_pp`, `tracking`.`mania_rank`, 
+                             `tracking`.`nowplaying` FROM `users` , `settings` , `tracking` WHERE users.user_id = %s
+                             ''',
+                             [user_id]).fetchone()
 
-        find_settings = mysql.execute(connection, cursor,
-                                      "SELECT * FROM settings WHERE user_id = %s",
-                                      [user_id]).fetchone()
-
-        find_tracking = mysql.execute(connection, cursor,
-                                      "SELECT * FROM tracking WHERE user_id = %s",
-                                      [user_id]).fetchone()
-
-        full.update(find_user)
-        full.update(find_settings)
-        full.update(find_tracking)
-        full.update(group.get_group(find_settings["privileges"]))
+        full.update(find)
+        full.update(group.get_group(find["privileges"]))
 
         return jsonify(full)
     else:
